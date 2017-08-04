b857a4225d4c8b62bc35db2713a61a0db2a952e9
gdiao <gary.diao@yahoo.com>
2017-08-04 19:29:17 +0800

    update

--- src/main/java/com/gp/sync/web/socket/GreetingController.java
+++ src/main/java/com/gp/sync/web/socket/GreetingController.java
@@ -1,6 +1,5 @@
 package com.gp.sync.web.socket;
 
-import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
