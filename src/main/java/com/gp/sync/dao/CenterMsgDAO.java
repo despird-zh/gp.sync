b00df51e0d209d5edef7e82c312a1cf00aa5728c
gdiao <gary.diao@yahoo.com>
2017-08-14 17:45:40 +0800

    update

--- src/main/java/com/gp/sync/dao/CenterMsgDAO.java
+++ src/main/java/com/gp/sync/dao/CenterMsgDAO.java
@@ -1,5 +1,36 @@
 package com.gp.sync.dao;
 
-public interface CenterMsgDAO {
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import org.springframework.jdbc.core.RowMapper;
+
+import com.gp.dao.BaseDAO;
+import com.gp.info.InfoId;
+import com.gp.sync.SyncIdKey;
+import com.gp.sync.dao.info.CenterMsgInfo;
+
+public interface CenterMsgDAO extends BaseDAO<CenterMsgInfo>{
+
+	public static RowMapper<CenterMsgInfo> CMSG_Mapper = new RowMapper<CenterMsgInfo>() {
+
+		@Override
+		public CenterMsgInfo mapRow(ResultSet rs, int arg1) throws SQLException {
+			CenterMsgInfo info  = new CenterMsgInfo();
+			
+			InfoId<Long> id = SyncIdKey.CEN_MSG.getInfoId(rs.getLong("msg_id"));
+			info.setInfoId(id);
+			info.setReceiveId(rs.getLong("rcv_id"));
+			info.setEntityCode(rs.getString("entity_code"));
+			info.setNodeCode(rs.getString("node_code"));
+			info.setTraceCode(rs.getString("trace_code"));
+			info.setOwm(rs.getLong("owm"));
+			info.setSyncCommand(rs.getString("sync_cmd"));
+			info.setMsgData(rs.getString("msg_data"));
+			
+			info.setModifier(rs.getString("modifier"));
+			info.setModifyDate(rs.getTimestamp("last_modified"));
+			
+			return info;
+		}};
 }
