ed79709827bedc31f1d8b7e08825031572e45aa9
gdiao <gary.diao@yahoo.com>
2017-08-15 10:18:19 +0800

    update

--- src/main/java/com/gp/sync/dao/NodeMsgInDAO.java
+++ src/main/java/com/gp/sync/dao/NodeMsgInDAO.java
@@ -1,5 +1,36 @@
 package com.gp.sync.dao;
 
-public interface NodeMsgInDAO {
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import org.springframework.jdbc.core.RowMapper;
+
+import com.gp.dao.BaseDAO;
+import com.gp.info.InfoId;
+import com.gp.sync.SyncIdKey;
+import com.gp.sync.dao.info.NodeMsgInInfo;
+
+public interface NodeMsgInDAO extends BaseDAO<NodeMsgInInfo>{
+
+	public static RowMapper<NodeMsgInInfo> MAPPER = new RowMapper<NodeMsgInInfo>() {
+
+		@Override
+		public NodeMsgInInfo mapRow(ResultSet rs, int arg1) throws SQLException {
+			NodeMsgInInfo info = new NodeMsgInInfo();
+			
+			InfoId<Long> id = SyncIdKey.NODE_MSG_IN.getInfoId(rs.getLong("msg_id"));
+			info.setInfoId(id);
+			info.setPullId(rs.getLong("pull_id"));
+			info.setEntityCode(rs.getString("entity_code"));
+			info.setNodeCode(rs.getString("node_code"));
+			info.setTraceCode(rs.getString("trace_code"));
+			info.setOwm(rs.getLong("owm"));
+			info.setSyncCommand(rs.getString("sync_cmd"));
+			info.setMsgData(rs.getString("msg_data"));
+			
+			info.setModifier(rs.getString("modifier"));
+			info.setModifyDate(rs.getTimestamp("last_modified"));
+			
+			return info;
+		}};
 }
