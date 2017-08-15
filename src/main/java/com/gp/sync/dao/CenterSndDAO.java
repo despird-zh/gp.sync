8d71bef92395fda10772c27b328ecf77729135b6
gdiao <gary.diao@yahoo.com>
2017-08-15 09:39:44 +0800

    update

--- src/main/java/com/gp/sync/dao/CenterSndDAO.java
+++ src/main/java/com/gp/sync/dao/CenterSndDAO.java
@@ -1,5 +1,33 @@
 package com.gp.sync.dao;
 
-public interface CenterSndDAO {
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import org.springframework.jdbc.core.RowMapper;
+
+import com.gp.dao.BaseDAO;
+import com.gp.info.InfoId;
+import com.gp.sync.SyncIdKey;
+import com.gp.sync.dao.info.CenterSndInfo;
+
+public interface CenterSndDAO extends BaseDAO<CenterSndInfo>{
+
+	public static RowMapper<CenterSndInfo> MAPPER = new RowMapper<CenterSndInfo>() {
+
+		@Override
+		public CenterSndInfo mapRow(ResultSet rs, int arg1) throws SQLException {
+			CenterSndInfo info = new CenterSndInfo();
+			
+			InfoId<Long> id = SyncIdKey.CEN_SND.getInfoId(rs.getLong("rcv_id"));
+			info.setInfoId(id);
+			info.setEntityCode(rs.getString("entity_code"));
+			info.setNodeCode(rs.getString("node_code"));
+			info.setSendTime(rs.getTimestamp("snd_time"));
+			info.setBloomData(rs.getString("bloom_data"));
+			info.setState(rs.getString("state"));
+			
+			info.setModifier(rs.getString("modifier"));
+			info.setModifyDate(rs.getTimestamp("last_modified"));
+			return info;
+		}};
 }
--- src/main/java/com/gp/sync/dao/impl/CenterSndDAOImpl.java
+++ src/main/java/com/gp/sync/dao/impl/CenterSndDAOImpl.java
@@ -20,37 +20,37 @@ import com.gp.config.ServiceConfigurer;
 import com.gp.dao.impl.DAOSupport;
 import com.gp.info.FlatColLocator;
 import com.gp.info.InfoId;
-import com.gp.sync.dao.CenterRcvDAO;
-import com.gp.sync.dao.info.CenterDistInfo;
-import com.gp.sync.dao.info.CenterRcvInfo;
+import com.gp.sync.dao.CenterSndDAO;
+import com.gp.sync.dao.info.CenterSndInfo;
 
 @Component
-public class CenterRcvDAOImpl extends DAOSupport implements CenterRcvDAO{
+public class CenterSndDAOImpl extends DAOSupport implements CenterSndDAO{
 
 	Logger LOGGER = LoggerFactory.getLogger(CenterRcvDAOImpl.class);
 	
 	@Autowired
-	public CenterRcvDAOImpl(@Qualifier(ServiceConfigurer.DATA_SRC) DataSource dataSource) {
+	public CenterSndDAOImpl(@Qualifier(ServiceConfigurer.DATA_SRC) DataSource dataSource) {
 		setDataSource(dataSource);
 	}
 	
 	@Override
-	public int create(CenterRcvInfo info) {
+	public int create(CenterSndInfo info) {
+		
 		StringBuffer SQL = new StringBuffer();
-		SQL.append("insert into gp_center_rcv (")
-			.append("rcv_id, entity_code, node_code, rcv_time, ")
-			.append("state, rcv_data, start_owm, end_owm, ")
+		SQL.append("insert into gp_center_snd (")
+			.append("snd_id, entity_code, node_code, snd_time, ")
+			.append("bloom_data, snd_data, state, ")
 			.append("modifier, last_modified")
 			.append(")values(")
 			.append("?,?,?,?,")
-			.append("?,?,?,?,")
+			.append("?,?,?,")
 			.append("?,?)");
 		
 		InfoId<Long> key = info.getInfoId();
 		
 		Object[] params = new Object[]{
-				key.getId(),info.getEntityCode(), info.getNodeCode(), info.getReceiveTime(),
-				info.getState(), info.getReceiveData(), info.getStartOwm(), info.getEndOwm(),
+				key.getId(),info.getEntityCode(), info.getNodeCode(), info.getSendTime(),
+				info.getBloomData(), info.getSendData(), info.getState(),
 				info.getModifier(),info.getModifyDate(),
 		};
 		if(LOGGER.isDebugEnabled()){
@@ -65,8 +65,8 @@ public class CenterRcvDAOImpl extends DAOSupport implements CenterRcvDAO{
 	@Override
 	public int delete(InfoId<?> id) {
 		StringBuffer SQL = new StringBuffer();
-		SQL.append("delete from gp_center_rcv ")
-			.append("where rcv_id = ? ");
+		SQL.append("delete from gp_center_snd ")
+			.append("where snd_id = ? ");
 		
 		JdbcTemplate jtemplate = this.getJdbcTemplate(JdbcTemplate.class);
 		Object[] params = new Object[]{
@@ -80,12 +80,12 @@ public class CenterRcvDAOImpl extends DAOSupport implements CenterRcvDAO{
 	}
 
 	@Override
-	public int update(CenterRcvInfo info, FilterMode mode, FlatColLocator... filterCols) {
+	public int update(CenterSndInfo info, FilterMode mode, FlatColLocator... filterCols) {
 		Set<String> colset = FlatColumns.toColumnSet(filterCols);
 		List<Object> params = new ArrayList<Object>();
 	
 		StringBuffer SQL = new StringBuffer();
-		SQL.append("update gp_center_rcv set ");
+		SQL.append("update gp_center_snd set ");
 		
 		if(columnCheck(mode, colset, "entity_code")){
 			SQL.append("entity_code = ? , ");
@@ -95,25 +95,21 @@ public class CenterRcvDAOImpl extends DAOSupport implements CenterRcvDAO{
 			SQL.append("node_code = ?, ");
 			params.add(info.getNodeCode());
 		}
-		if(columnCheck(mode, colset, "rcv_time")){
-			SQL.append("rcv_time = ?,");
-			params.add(info.getReceiveTime());
+		if(columnCheck(mode, colset, "snd_time")){
+			SQL.append("snd_time = ?,");
+			params.add(info.getSendTime());
 		}
 		if(columnCheck(mode, colset, "state")){
 			SQL.append("state = ?, ");
 			params.add(info.getState());
 		}
-		if(columnCheck(mode, colset, "rcv_data")){
-			SQL.append("rcv_data = ?, ");
-			params.add(info.getReceiveData());
+		if(columnCheck(mode, colset, "snd_data")){
+			SQL.append("snd_data = ?, ");
+			params.add(info.getSendData());
 		}
-		if(columnCheck(mode, colset, "start_owm")){
-			SQL.append("start_owm = ?, ");
-			params.add(info.getStartOwm());
-		}
-		if(columnCheck(mode, colset, "end_owm")){
-			SQL.append("end_owm = ?, ");
-			params.add(info.getEndOwm());
+		if(columnCheck(mode, colset, "bloom_data")){
+			SQL.append("bloom_data = ?, ");
+			params.add(info.getBloomData());
 		}
 		
 		SQL.append("modifier = ?, last_modified = ? ")
@@ -131,9 +127,9 @@ public class CenterRcvDAOImpl extends DAOSupport implements CenterRcvDAO{
 	}
 
 	@Override
-	public CenterRcvInfo query(InfoId<?> id) {
-		String SQL = "select * from gp_center_rcv "
-				+ "where rcv_id = ? ";
+	public CenterSndInfo query(InfoId<?> id) {
+		String SQL = "select * from gp_center_snd "
+				+ "where snd_id = ? ";
 		
 		Object[] params = new Object[]{				
 				id.getId()
@@ -143,7 +139,7 @@ public class CenterRcvDAOImpl extends DAOSupport implements CenterRcvDAO{
 		if(LOGGER.isDebugEnabled()){			
 			LOGGER.debug("SQL : " + SQL.toString() + " / params : " + ArrayUtils.toString(params));
 		}
-		List<CenterRcvInfo> ainfo = jtemplate.query(SQL, params, MAPPER);
+		List<CenterSndInfo> ainfo = jtemplate.query(SQL, params, MAPPER);
 		
 		return ainfo.size() > 0 ? ainfo.get(0) : null;
 	}
