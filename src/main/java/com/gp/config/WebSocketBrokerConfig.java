c6e3a8f097f2d5b13f628801092cdcd6cece5aa4
gdiao <gary.diao@yahoo.com>
2017-08-31 20:18:18 +0800

    fupdate

--- src/main/java/com/gp/config/WebSocketBrokerConfig.java
+++ src/main/java/com/gp/config/WebSocketBrokerConfig.java
@@ -2,7 +2,6 @@ package com.gp.config;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
