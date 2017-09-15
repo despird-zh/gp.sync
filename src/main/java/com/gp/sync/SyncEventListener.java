d900f821dbcf65eba9aede9e7641730369e578fc
gdiao <gary.diao@yahoo.com>
2017-09-15 08:47:37 +0800

    change hooker to listener

--- src/main/java/com/gp/sync/SyncEventListener.java
+++ src/main/java/com/gp/sync/SyncEventListener.java
@@ -3,20 +3,20 @@ package com.gp.sync;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.gp.disruptor.EventHooker;
+import com.gp.disruptor.EventListener;
 import com.gp.disruptor.EventPayload;
 import com.gp.disruptor.EventType;
 import com.gp.exception.RingEventException;
 
-public class SyncEventHooker extends EventHooker<SyncEventLoad>{
+public class SyncEventListener extends EventListener<SyncEventLoad>{
 
-	public static Logger LOGGER = LoggerFactory.getLogger(SyncEventHooker.class);
+	public static Logger LOGGER = LoggerFactory.getLogger(SyncEventListener.class);
 	
-	public SyncEventHooker() {
+	public SyncEventListener() {
 		this(EventType.SYNC);
 	}
 	
-	public SyncEventHooker(EventType eventType) {
+	public SyncEventListener(EventType eventType) {
 		super(eventType);
 	}
 
